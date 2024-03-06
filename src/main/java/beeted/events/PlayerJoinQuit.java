e0aa25a7edca4df5a7f37f6b66917ae3dd37c865
Btd20 <bergahd@gmail.com>
2024-03-06 15:55:17 +0100

    Bug Fixed.

--- src/main/java/beeted/events/PlayerJoinQuit.java
+++ src/main/java/beeted/events/PlayerJoinQuit.java
@@ -2,7 +2,6 @@ package beeted.events;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import beeted.customwelcome.CustomWelcome;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -20,37 +19,29 @@ public class PlayerJoinQuit implements Listener {
     public PlayerJoinQuit(CustomWelcome plugin) {
         this.plugin = plugin;
     }
+
     @EventHandler
     public void Join(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         FileConfiguration config = plugin.getConfig();
-        String path = "config.enable-join-message";
         String path2 = "config.enable-first-join-message";
 
-        if(config.getString(path2).equals("true")) {
-            if(!player.hasPlayedBefore()) {
-                final int Count = plugin.getConfig().getInt("player-count");
-                plugin.getConfig().set("player-count", Count + 1);
-                plugin.saveConfig();
-                String text = "config.first-join-message";
-                String message = config.getString(text);
-                if (message.contains("%player_count%")) {
-                    message = message.replace("%player_count%", String.valueOf(Count));
-                }
-                message = PlaceholderAPI.setPlaceholders(player, message);
-                event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', message));
+        if (config.getString(path2).equals("true") && !player.hasPlayedBefore()) {
+            final int Count = plugin.getConfig().getInt("player-count");
+            plugin.getConfig().set("player-count", Count + 1);
+            plugin.saveConfig();
+            String text = "config.first-join-message";
+            String message = config.getString(text);
+            if (message.contains("%player_count%")) {
+                message = message.replace("%player_count%", String.valueOf(Count));
             }
-        } else {
-            event.setJoinMessage(null);
-        }
-
-        if (config.getString(path).equals("true")) {
-            if (player.hasPlayedBefore()) {
-                String text = "config.join-message";
-                String message = config.getString(text);
-                message = PlaceholderAPI.setPlaceholders(player, message);
-                event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', message));
-            }
+            message = PlaceholderAPI.setPlaceholders(player, message);
+            event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', message));
+        } else if (config.getString("config.enable-join-message").equals("true") && player.hasPlayedBefore()) {
+            String text = "config.join-message";
+            String message = config.getString(text);
+            message = PlaceholderAPI.setPlaceholders(player, message);
+            event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', message));
         } else {
             event.setJoinMessage(null);
         }
@@ -61,12 +52,10 @@ public class PlayerJoinQuit implements Listener {
             String text = "motd.motd-message";
             List<String> messages = plugin.getConfig().getStringList(text);
 
-            for (int i = 0; i < messages.size(); i++) {
-                String motdtext = PlaceholderAPI.setPlaceholders(player, messages.get(i));
+            for (String motdtext : messages) {
+                motdtext = PlaceholderAPI.setPlaceholders(player, motdtext);
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', motdtext));
             }
-        } else {
-            event.setJoinMessage(null);
         }
     }
 
